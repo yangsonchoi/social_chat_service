c166110b0a6550b1045fe0999058d98ab40534d9
Yangson Andrew Choi <yachoi@Yangsons-MacBook-Pro.local>
2023-12-10 02:27:56 +0900

    [FEAT] Add unique constraint to username and test
    Add unique constraint to username and tested
    nullable: false constraint doesn't work
    may need to check in repository layer

--- backend/src/entities/userEntity.test.ts
+++ backend/src/entities/userEntity.test.ts
@@ -21,18 +21,84 @@ describe("User entity test", () => {
   });
 
   it("Should create a user", async () => {
-    const userSave = userRepository.create({ username: "testuser" });
+    // Create a user
+    const userSave = await userRepository.create({ username: "testuser" });
     await userRepository.save(userSave);
 
+    // User id should be generated
     expect(userSave.id).toBeDefined();
 
+    // Get user
     const userLoad = await userRepository.findOne({
       where: { id: userSave.id },
     });
 
+    // Check user
     expect(userLoad).toBeTruthy();
     if (userLoad) {
       expect(userLoad.username).toBe("testuser");
     }
   });
+
+  it("should not allow duplicate usernames", async () => {
+    // Create the first user
+    const user1 = await userRepository.create({
+      username: "uniqueUser",
+      password: "password123",
+    });
+    await userRepository.save(user1);
+
+    // Try to create same user
+    try {
+      const user2 = await userRepository.create({
+        username: "uniqueUser",
+        password: "password123",
+      });
+      await userRepository.save(user2);
+
+      // If the save succeeds, the test should fail
+      fail("Should not allow two users with the same username");
+    } catch (error) {
+      if (error instanceof Error) {
+        expect(error.message).toMatch(/ER_DUP_ENTRY/);
+      }
+    }
+  });
+
+  // it("should not allow creating a user without a username", async () => {
+  //   try {
+  //     const user = userRepository.create({ password: "password123" });
+  //     await userRepository.save(user);
+
+  //     // If the save succeeds, the test should fail
+  //     fail("should not allow creating a user without a username");
+  //   } catch (error) {
+  //     if (error instanceof Error) {
+  //       expect(error.message).toMatch(
+  //         /column "username" violates not-null constraint/
+  //       );
+  //     }
+  //   }
+  // });
+
+  // it("should not allow creating a user without a password", async () => {
+  //   try {
+  //     const user = userRepository.create({ username: "testuser2" });
+  //     await userRepository.save(user);
+
+  //     console.log(user)
+  //     const userLoad = await userRepository.findOne({
+  //       where: { id: user.id },
+  //     });
+  //     console.log(userLoad);
+  //     // If the save succeeds, the test should fail
+  //     fail("should not allow creating a user without a password");
+  //   } catch (error) {
+  //     if (error instanceof Error) {
+  //       expect(error.message).toMatch(
+  //         /column "password" violates not-null constraint/
+  //       );
+  //     }
+  //   }
+  // });
 });
--- backend/src/entities/userEntity.ts
+++ backend/src/entities/userEntity.ts
@@ -1,16 +1,21 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from "typeorm";
 
 @Entity()
 export class User {
-    @PrimaryGeneratedColumn()
-    id!: number;
+  @PrimaryGeneratedColumn()
+  id!: number;
 
-    @Column()
-    username!: string;
+  @Column({ unique: true, nullable: false })
+  username!: string;
 
-    @Column()
-    password!: string;
+  @Column({ nullable: false })
+  password!: string;
 
-    @CreateDateColumn()
-    createdAt!: Date;
+  @CreateDateColumn()
+  createdAt!: Date;
 }
