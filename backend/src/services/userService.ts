7a5f054799d260440330871107b12ae1125694ea
Yangson Andrew Choi <yachoi@Yangsons-MacBook-Pro.local>
2023-12-10 17:37:19 +0900

    [FEAT] Add user service
    createUser, getUserById, getUserByName, getAllUsers

---
+++ backend/src/services/userService.ts
@@ -0,0 +1,28 @@
+import { UserRepository } from "../repositories/userRepository";
+import { User } from "../entities/userEntity";
+import { DataSource } from "typeorm";
+
+export class UserService {
+  private userRepository: UserRepository;
+
+  constructor(dataSource: DataSource) {
+    this.userRepository = new UserRepository(dataSource);
+  }
+
+  async createUser(userData: User): Promise<User> {
+    return this.userRepository.saveUser(userData);
+  }
+
+  async getUserById(id: number): Promise<User | null> {
+    return this.userRepository.findUserById(id);
+  }
+
+  async getUserByName(name: string): Promise<User | null> {
+    return this.userRepository.findUserByName(name);
+  }
+
+  async getAllUsers(): Promise<User[]> {
+    return this.userRepository.findAllUsers();
+  }
+
+}
