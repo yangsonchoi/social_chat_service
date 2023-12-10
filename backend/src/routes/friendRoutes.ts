72e31441a940ebc77e2c44a419bce46db08be476
Yangson Andrew Choi <yachoi@Yangsons-MacBook-Pro.local>
2023-12-11 03:27:13 +0900

    [FEAT] add get freind lists to friend router

--- backend/src/routes/friendRoutes.ts
+++ backend/src/routes/friendRoutes.ts
@@ -5,6 +5,10 @@ import { AppDataSource } from "../config/typeOrmConfig";
 const router = express.Router();
 const friendController = new FriendController(AppDataSource);
 
+router.get("/", (req: Request, res: Response) => {
+  friendController.getAllFriendsList(req, res);
+});
+
 router.post("/request", (req: Request, res: Response) => {
   friendController.requestFriend(req, res);
 });
@@ -16,5 +20,14 @@ router.put("/accept", (req: Request, res: Response) => {
 router.put("/decline", (req: Request, res: Response) => {
   friendController.declineFriend(req, res);
 });
+
+router.get("/requested", (req: Request, res: Response) => {
+  friendController.getRequestedFriends(req, res);
+});
+
+router.get("/addressed", (req: Request, res: Response) => {
+  friendController.getAddressedFriends(req, res);
+});
+
 
 export default router;
