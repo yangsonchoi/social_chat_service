38d2ad516548d6190a2f8e1d9b2d6c4caebcf74d
Yangson Andrew Choi <yachoi@Yangsons-MacBook-Pro.local>
2023-12-11 03:25:03 +0900

    [FEAT] add getAllFriendsList and more to friend repository

--- backend/src/repositories/friendRepository.ts
+++ backend/src/repositories/friendRepository.ts
@@ -11,12 +11,21 @@ export class FriendRepository {
     this.friendRequestRepository = dataSource.getRepository(FriendRequest);
   }
 
-  async makeFriendRequest(requesterId: number, addresseeId: number): Promise<FriendRequest> {
-    const friendRequest = this.friendRequestRepository.create({ requesterId, addresseeId });
+  async makeFriendRequest(
+    requesterId: number,
+    addresseeId: number
+  ): Promise<FriendRequest> {
+    const friendRequest = this.friendRequestRepository.create({
+      requesterId,
+      addresseeId,
+    });
     return this.friendRequestRepository.save(friendRequest);
   }
 
-  async deleteFriendRequest(requesterId: number, addresseeId: number): Promise<void> {
+  async deleteFriendRequest(
+    requesterId: number,
+    addresseeId: number
+  ): Promise<void> {
     await this.friendRequestRepository.delete({ requesterId, addresseeId });
   }
 
@@ -31,10 +40,55 @@ export class FriendRepository {
 
   async countUserFriendships(userId: number): Promise<number> {
     const count = await this.friendshipRepository
-        .createQueryBuilder("friendship")
-        .where("friendship.user1Id = :userId OR friendship.user2Id = :userId", { userId })
-        .getCount();
+      .createQueryBuilder("friendship")
+      .where("friendship.user1Id = :userId OR friendship.user2Id = :userId", {
+        userId,
+      })
+      .getCount();
 
     return count;
+  }
+
+  async getAllFriendsList(userId: number): Promise<number[]> {
+    const friendships = await this.friendshipRepository
+      .createQueryBuilder("friendship")
+      .where("friendship.user1Id = :userId", { userId })
+      .orWhere("friendship.user2Id = :userId", { userId })
+      .getMany();
+
+    const friendIds = friendships.flatMap((friendship) => {
+      return [
+        friendship.user1Id === userId ? friendship.user2Id : null,
+        friendship.user2Id === userId ? friendship.user1Id : null,
+      ].filter((id) => id !== null) as number[];
+    });
+
+    console.log(friendIds);
+
+    return Array.from(new Set(friendIds));
+  }
+
+  async getAllFriendRequestsList(userId: number): Promise<number[]> {
+    const friendRequests = await this.friendRequestRepository
+      .createQueryBuilder("friendRequest")
+      .where("friendRequest.requesterId = :userId", { userId })
+      .select("friendRequest.addresseeId")
+      .getMany();
+
+    const addresseeIds = friendRequests.map((fr) => fr.addresseeId);
+
+    return Array.from(addresseeIds);
+  }
+
+  async getAllAddressedFriendsList(userId: number): Promise<number[]> {
+    const friendAddressed = await this.friendRequestRepository
+      .createQueryBuilder("friendRequest")
+      .where("friendRequest.addresseeId = :userId", { userId })
+      .select("friendRequest.requesterId")
+      .getMany();
+
+    const requesterIds = friendAddressed.map((fr) => fr.requesterId);
+
+    return Array.from(requesterIds);
   }
 }
