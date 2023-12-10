6c1bbb4ca8bdf89db98d592e97a1d42c4bc52190
Yangson Andrew Choi <yachoi@Yangsons-MacBook-Pro.local>
2023-12-11 03:26:36 +0900

    [FEAT] Add getAllFriendsList and more to friend controller

--- backend/src/controllers/friendController.ts
+++ backend/src/controllers/friendController.ts
@@ -2,12 +2,16 @@ import { FriendService } from "../services/friendService";
 import { Request, Response } from "express";
 import { DataSource } from "typeorm";
 import { getUserIdFromToken } from "../utils/tokenVerfy";
+import { UserService } from "../services/userService";
 
 export class FriendController {
   private friendService: FriendService;
+  private userService: UserService;
 
   constructor(dataSource: DataSource) {
     this.friendService = new FriendService(dataSource);
+    this.userService = new UserService(dataSource);
+
   }
 
   async requestFriend(req: Request, res: Response) {
@@ -47,6 +51,51 @@ export class FriendController {
         const requesterId = parseInt(req.body.id);
         await this.friendService.declineFriend(requesterId, addresseeId);
         res.status(200).send('Friend request declined');
+      } catch (error) {
+        res.status(500).json({ error: error instanceof Error ? error.message : "Unknown error" });
+      }
+    } else {
+      res.status(401).json({ message: "Unauthorized" });
+    }
+  }
+
+  async getAllFriendsList(req: Request, res: Response) {
+    const userId = getUserIdFromToken(req);
+    if (!userId) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    try {
+      const friendIds = await this.friendService.getAllFriendsList(userId);
+      const friends = await this.userService.getUsersByIds(friendIds);
+      res.status(200).json(friends);
+    } catch (error) {
+      res.status(500).json({ error: error instanceof Error ? error.message : "Unknown error" });
+    }
+  }
+
+  async getRequestedFriends(req: Request, res: Response) {
+    const userId = getUserIdFromToken(req);
+    if (userId) {
+      try {
+        const requestedFriendsIds = await this.friendService.getAllFriendRequestsList(userId);
+        const requestedFriends = await this.userService.getUsersByIds(requestedFriendsIds);
+        res.status(200).json(requestedFriends);
+      } catch (error) {
+        res.status(500).json({ error: error instanceof Error ? error.message : "Unknown error" });
+      }
+    } else {
+      res.status(401).json({ message: "Unauthorized" });
+    }
+  }
+
+  async getAddressedFriends(req: Request, res: Response) {
+    const userId = getUserIdFromToken(req);
+    if (userId) {
+      try {
+        const addressedFriendsIds = await this.friendService.getAllAddressedFriendsList(userId);
+        const addressedFriends = await this.userService.getUsersByIds(addressedFriendsIds);
+        res.status(200).json(addressedFriends);
       } catch (error) {
         res.status(500).json({ error: error instanceof Error ? error.message : "Unknown error" });
       }
